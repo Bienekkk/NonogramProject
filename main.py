566c4da4102c399286b9ed13ffa761c9bae4189c
Bienek <pawelgarbien.2112@gmail.com>
2024-11-26 22:16:15 +0100

    index styles

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/game')
+def game():
+    return render_template('game.html')
 
 
 if __name__ == '__main__':
