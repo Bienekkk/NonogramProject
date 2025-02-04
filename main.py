70216cc7a8ca75f4c1e0a87c69f3da365fef4974
Bienek <pawelgarbien.2112@gmail.com>
2025-02-04 22:15:31 +0100

    Normal mode mechanics done

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import random
+
 from flask import Flask
 from flask import render_template, url_for, request
 import os
+import json
 
 app = Flask(__name__)
 
@@ -11,7 +14,16 @@ def index():
 @app.route('/play')
 def play():
     mode = request.args.get("mode")
-    return render_template('play.html', mode=mode)
+    mode = int(mode)
+    with open("static/src/layouts.json", 'r') as layouts:
+        data = json.load(layouts)
+    data = (data[str(mode)])
+    ran = random.randint(1, len(data))
+    return render_template('play.html', mode=mode, data=data[str(ran)])
+
+@app.route('/temp')
+def temp():
+    return render_template('temporary.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/scripts/script-play.js
@@ -0,0 +1,143 @@
+let colDiv = document.querySelector('.colDiv');
+let rowDiv = document.querySelector('.rowDiv');
+let fields = document.querySelectorAll('.field');
+let checkbox = document.getElementById('checkbox');
+let endDiv = document.querySelector('.endDiv');
+let endText = document.querySelector('.endText');
+let imgDiv = document.querySelector('.imgDiv');
+
+let jsonstr = document.querySelector('.data').innerText;
+jsonstr = jsonstr.replaceAll("'", '"');
+
+let json = JSON.parse(jsonstr);
+console.log(json);
+
+let arr = json["arr"];
+let targetAmount = 0;
+let checkedAmount = 0;
+for (let i = 0; i < arr.length; i++){
+    if(arr[i]==1){
+        targetAmount++;
+    }
+}
+let cols = json["col"];
+for (let i = 0; i < cols.length; i++) {
+    const col = document.createElement("p");
+    col.classList.add('col');
+    col.setAttribute('draggable', false);
+    cols[i].forEach((element) => col.innerHTML += (element + "<br>"));
+    colDiv.appendChild(col);
+}
+
+let rows = json["row"];
+for (let i = 0; i < rows.length; i++) {
+    const row = document.createElement("p");
+    row.classList.add('row');
+    row.setAttribute('draggable', false);
+    rows[i].forEach((element) => row.innerText += (element + " "));
+    rowDiv.appendChild(row);
+}
+
+let hearts = 3;
+let wait = false;
+
+//click actions
+let isMouseDown = false;
+fields.forEach((field) => {
+    field.addEventListener("dragstart", (event) => {
+        event.preventDefault();
+    });
+    field.addEventListener("mousedown", () => {
+        isMouseDown = true;
+        checkAction(field)
+    });
+    field.addEventListener("mousemove", () => {
+        if (isMouseDown) {
+            checkAction(field)
+        }
+    });
+    field.addEventListener("mouseup", () => {
+        isMouseDown = false;
+    });
+});
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+});
+document.addEventListener("selectstart", (event) => {
+    event.preventDefault();
+});
+
+async function checkAction(field){
+    if(wait || field.style.backgroundColor === 'rgb(30, 30, 30)' || field.hasChildNodes()){
+        return;
+    }
+
+    if(arr[field.id] === 1){
+        field.style.backgroundColor = "#1E1E1E";
+        updateClicked();
+
+        if(checkbox.checked===false){
+            updateHearts();
+        }
+    }
+    else if(arr[field.id] === 0){
+        const x = document.createElement('i');
+        x.classList.add('bi');
+        x.classList.add('bi-x-lg');
+        field.appendChild(x);
+
+        if(checkbox.checked===true){
+            updateHearts();
+        }
+    }
+    else{
+        updateHearts();
+    }
+}
+
+async function updateHearts(){
+    wait = true;
+    let heart = document.getElementById(`heart${hearts}`)
+    heart.style.filter = 'grayscale(1)'
+    hearts--;
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    if(hearts<=0){
+        endGame("You lost!!!")
+    }
+    wait = false;
+}
+
+async function updateClicked(){
+    checkedAmount++;
+    if(checkedAmount==targetAmount){
+        await new Promise(resolve => setTimeout(resolve, 500));
+        endGame("You win!!!")
+    }
+}
+
+function reset(){
+    window.location.reload();
+}
+
+async function endGame(text){
+    if(text==="You win!!!"){
+        wait = true;
+        imgDiv.style.display = "block";
+        imgDiv.style.opacity = 1;
+        const img = document.createElement('img');
+        img.classList.add('img');
+        img.style.animation = "opacity 1s linear 1 forwards";
+        let imgSrc = './static/src/img/' + json['img'];
+        img.src = imgSrc;
+        imgDiv.appendChild(img);
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        endDiv.style.display = 'block';
+        endText.innerText = text;
+    }
+    else{
+        wait = true;
+        endDiv.style.display = 'block';
+        endText.innerText = text;
+    }
+}
