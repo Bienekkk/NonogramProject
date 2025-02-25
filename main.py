a7ab0973938c07a7108eb6f9811bcad58ad3809b
Bienek <pawelgarbien.2112@gmail.com>
2025-02-25 21:16:26 +0100

    image send to backend

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import random
 
+import flask
 from flask import Flask
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, jsonify
 import os
 import json
+
+# from PIL import Image
+# from itertools import product
 
 app = Flask(__name__)
 
@@ -21,9 +25,45 @@ def play():
     ran = random.randint(1, len(data))
     return render_template('play.html', mode=mode, data=data[str(ran)])
 
+@app.route('/your_image', methods=['POST'])
+def your_image():
+    try:
+        if 'imagefile' not in request.files:
+            return jsonify({"error": "No file part"}), 400
+
+        imagefile = request.files['imagefile']
+
+        print(imagefile)
+
+        if imagefile.filename == '':
+            return jsonify({"error": "No selected file"}), 400
+
+        # Save the file (optional)
+        imagefile.save(f"uploads/{imagefile.filename}")
+
+        return jsonify({"message": "Image uploaded successfully", "filename": imagefile.filename})
+
+    except Exception as err:
+        print(err)
+        return jsonify({"error": str(err)}), 500
+
 @app.route('/temp')
 def temp():
     return render_template('temporary.html')
+
+
+# def tile(filename, dir_in, dir_out, d):
+#     name, ext = os.path.splitext(filename)
+#     img = Image.open(os.path.join(dir_in, filename))
+#     w, h = img.size
+#
+#     grid = product(range(0, h - h % d, d), range(0, w - w % d, d))
+#     for i, j in grid:
+#         box = (j, i, j + d, i + d)
+#         out = os.path.join(dir_out, f'{name}_{i}_{j}{ext}')
+#         img.crop(box).save(out)
+
+#https://stackoverflow.com/questions/5953373/how-to-split-image-into-multiple-pieces-in-python
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/scripts/script-play.js
+++ static/scripts/script-play.js
@@ -24,18 +24,18 @@ for (let i = 0; i < arr.length; i++){
 let cols = json["col"];
 for (let i = 0; i < cols.length; i++) {
     const col = document.createElement("p");
-    col.classList.add('col');
+    col.classList.add('col', `col${i}`);
     col.setAttribute('draggable', false);
-    cols[i].forEach((element, index) => col.innerHTML += `<span id="col${i}-${index}">${element}</span>` + '<br>'); //naprawić br
+    cols[i].forEach((element, index) => col.innerHTML += `<span class="col${i} colId${index}">${element}</span>`);
     colDiv.appendChild(col);
 }
 
 let rows = json["row"];
 for (let i = 0; i < rows.length; i++) {
     const row = document.createElement("p");
-    row.classList.add('row');
+    row.classList.add('row', `row${i}`);
     row.setAttribute('draggable', false);
-    rows[i].forEach((element) => row.innerText += (element + " "));
+    rows[i].forEach((element, index) => row.innerHTML += `<span class="row${i} rowId${index}">${element}</span>` + " ");
     rowDiv.appendChild(row);
 }
 
@@ -73,13 +73,19 @@ async function checkAction(field){
         return;
     }
 
+    const col = field.id % mode;
+    const row = Math.floor(field.id / mode);
+
     if(arr[field.id] === 1){
         field.style.backgroundColor = "#1E1E1E";
+        field.classList.add("checked");
         updateClicked();
 
         if(checkbox.checked===false){
             updateHearts();
         }
+        checkAllRow(row)
+        checkAllCol(col)
     }
     else if(arr[field.id] === 0){
         const x = document.createElement('i');
@@ -95,11 +101,8 @@ async function checkAction(field){
         updateHearts();
     }
 
-    const col = field.id % mode;
-    const row = Math.floor(field.id / mode);
-    console.log(col, row);
     checkCol(col)
-    //-------------------------------------------------------------------------------------------------
+    checkRow(row)
 }
 
 async function updateHearts(){
@@ -153,14 +156,11 @@ async function checkCol(col){
     let counter = 0;
     let sequenceNr = 0;
     for(let i = col; i < mode*mode + col; i += mode){
-        //console.log(i)
         let field = document.getElementById(i);
         if(field.style.backgroundColor === 'rgb(30, 30, 30)'){
             counter ++;
-            console.log(counter)
-            //console.log(cols[col][sequenceNr])
             if(cols[col][sequenceNr] == counter){
-                document.getElementById(`col${col}-${sequenceNr}`).style.color = 'lightgray';
+                document.querySelector(`.col${col}.colId${sequenceNr}`).style.color = 'lightgray';
                 sequenceNr++;
             }
         }
@@ -170,5 +170,96 @@ async function checkCol(col){
         else{
             break;
         }
+    }
+    counter = 0;
+    sequenceNr = cols[col].length - 1;
+    for(let i = mode*mode + col - mode; i > 0; i -= mode){
+        let field = document.getElementById(i);
+        if(field.style.backgroundColor === 'rgb(30, 30, 30)'){
+            counter ++;
+            if(cols[col][sequenceNr] == counter){
+                document.querySelector(`.col${col}.colId${sequenceNr}`).style.color = 'lightgray';
+                sequenceNr--;
+            }
+        }
+        else if(field.hasChildNodes()){
+            counter = 0;
+        }
+        else{
+            break;
+        }
+    }
+}
+async function checkRow(row){
+    let counter = 0;
+    let sequenceNr = 0;
+    for(let i = row * mode; i < row * mode + mode; i += 1){
+        let field = document.getElementById(i);
+        if(field.style.backgroundColor === 'rgb(30, 30, 30)'){
+            counter ++;
+            if(rows[row][sequenceNr] == counter){
+                document.querySelector(`.row${row}.rowId${sequenceNr}`).style.color = 'lightgray';
+                sequenceNr++;
+            }
+        }
+        else if(field.hasChildNodes()){
+            counter = 0;
+        }
+        else{
+            break;
+        }
+    }
+    counter = 0;
+    sequenceNr = rows[row].length - 1;
+    for(let i = row * mode + mode - 1; i > row * mode; i -= 1){
+        let field = document.getElementById(i);
+        if(field.style.backgroundColor === 'rgb(30, 30, 30)'){
+            counter ++;
+            if(rows[row][sequenceNr] == counter){
+                document.querySelector(`.row${row}.rowId${sequenceNr}`).style.color = 'lightgray';
+                sequenceNr--;
+            }
+        }
+        else if(field.hasChildNodes()){
+            counter = 0;
+        }
+        else{
+            break;
+        }
+    }
+}
+async function checkAllRow(row){
+    let counter = 0;
+    let checked = 0;
+    rows[row].map((e) => {
+        checked += e;
+    })
+    //console.log(checked)
+    for(let i = row * mode; i < row * mode + mode; i += 1){
+        let field = document.getElementById(i);
+        if(field.style.backgroundColor === 'rgb(30, 30, 30)'){
+            counter ++;
+        }
+    }
+    if(counter == checked){
+        document.querySelector(`.row${row}`).style.color = "lightgray";
+    }
+}
+
+async function checkAllCol(col){
+    let counter = 0;
+    let checked = 0;
+    cols[col].map((e) => {
+        checked += e;
+    })
+    console.log(checked)
+    for(let i = col; i < mode*mode + col; i += mode){
+        let field = document.getElementById(i);
+        if(field.style.backgroundColor === 'rgb(30, 30, 30)'){
+            counter ++;
+        }
+    }
+    if(counter == checked){
+        document.querySelector(`.col${col}`).style.color = "lightgray";
     }
 }
