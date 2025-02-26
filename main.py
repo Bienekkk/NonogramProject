1787bb71b38b270a7d149bd8c1db76c313c04a58
Paweł Garbień <p_garbien@inf15.local>
2025-02-26 09:03:43 +0100

    crop image add

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import random
 import flask
 from flask import Flask
 from flask import render_template, url_for, request, jsonify
-import os
+import os, shutil, math
 import json
-
-# from PIL import Image
-# from itertools import product
+from PIL import Image
+from itertools import product
 
 app = Flask(__name__)
 
@@ -27,6 +26,7 @@ def play():
 
 @app.route('/your_image', methods=['POST'])
 def your_image():
+    clearUploads()
     try:
         if 'imagefile' not in request.files:
             return jsonify({"error": "No file part"}), 400
@@ -41,6 +41,8 @@ def your_image():
         # Save the file (optional)
         imagefile.save(f"uploads/{imagefile.filename}")
 
+        tile(imagefile.filename, 'uploads', 'uploads')
+
         return jsonify({"message": "Image uploaded successfully", "filename": imagefile.filename})
 
     except Exception as err:
@@ -52,18 +54,31 @@ def temp():
     return render_template('temporary.html')
 
 
-# def tile(filename, dir_in, dir_out, d):
-#     name, ext = os.path.splitext(filename)
-#     img = Image.open(os.path.join(dir_in, filename))
-#     w, h = img.size
-#
-#     grid = product(range(0, h - h % d, d), range(0, w - w % d, d))
-#     for i, j in grid:
-#         box = (j, i, j + d, i + d)
-#         out = os.path.join(dir_out, f'{name}_{i}_{j}{ext}')
-#         img.crop(box).save(out)
+def tile(filename, dir_in, dir_out):
+    name, ext = os.path.splitext(filename)
+    img = Image.open(os.path.join(dir_in, filename))
+    w, h = img.size
+    d = math.ceil(w / 11)
 
-#https://stackoverflow.com/questions/5953373/how-to-split-image-into-multiple-pieces-in-python
+    grid = product(range(0, h - h % d, d), range(0, w - w % d, d))
+    for i, j in grid:
+        box = (j, i, j + d, i + d)
+        out = os.path.join(dir_out, f'{name}_{i}_{j}{ext}')
+        img.crop(box).save(out)
+
+def clearUploads():
+    folder = 'uploads'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+#https://stackoverflow.com/questions/3241929/how-to-find-the-dominant-most-common-color-in-an-image
 
 if __name__ == '__main__':
     app.run(debug=True)
