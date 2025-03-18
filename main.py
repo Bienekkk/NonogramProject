f73c812daf3a3bd37b8f8fb08f2f4bc78edd6cf2
Bienek <pawelgarbien.2112@gmail.com>
2025-03-18 21:59:17 +0100

    your image crop and dominant color get

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+from __future__ import print_function
 import random
 
 import flask
+
 from flask import Flask
 from flask import render_template, url_for, request, jsonify
 import os, shutil, math
 import json
 from PIL import Image
 from itertools import product
+from colorthief import ColorThief
+
+
+# import binascii
+# import numpy as np
+# import scipy
+# import scipy.misc
+# import scipy.cluster
+
+NUM_CLUSTERS = 5
 
 app = Flask(__name__)
 
@@ -24,7 +36,7 @@ def play():
     ran = random.randint(1, len(data))
     return render_template('play.html', mode=mode, data=data[str(ran)])
 
-@app.route('/your_image', methods=['POST'])
+@app.route('/your_image',  methods=['POST'])
 def your_image():
     clearUploads()
     try:
@@ -41,13 +53,15 @@ def your_image():
         # Save the file (optional)
         imagefile.save(f"uploads/{imagefile.filename}")
 
-        tile(imagefile.filename, 'uploads', 'uploads')
-
-        return jsonify({"message": "Image uploaded successfully", "filename": imagefile.filename})
+        colors = tile(imagefile.filename, 'uploads', 'uploads')
+        print("render")
+        #return render_template('your_photo.html')
 
     except Exception as err:
-        print(err)
+        print("ErRoR", err)
         return jsonify({"error": str(err)}), 500
+
+    return render_template('your_photo.html', colors=colors)
 
 @app.route('/temp')
 def temp():
@@ -55,16 +69,45 @@ def temp():
 
 
 def tile(filename, dir_in, dir_out):
+    colors = []
     name, ext = os.path.splitext(filename)
     img = Image.open(os.path.join(dir_in, filename))
     w, h = img.size
-    d = math.ceil(w / 11)
+    d = math.floor(w / 10)
 
     grid = product(range(0, h - h % d, d), range(0, w - w % d, d))
     for i, j in grid:
         box = (j, i, j + d, i + d)
         out = os.path.join(dir_out, f'{name}_{i}_{j}{ext}')
         img.crop(box).save(out)
+
+    grid = product(range(0, h - h % d, d), range(0, w - w % d, d))
+    for i, j in grid:
+        color_thief = ColorThief(f'uploads/{name}_{i}_{j}{ext}')
+        dominant_color = color_thief.get_color(quality=10)
+        print(i, j, dominant_color)
+        colors.append(dominant_color)
+
+    return colors
+
+        #print('reading image')
+        # im = img
+        # im = im.resize((150, 150))  # optional, to reduce time
+        # ar = np.asarray(im)
+        # shape = ar.shape
+        # ar = ar.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
+        #
+        # print('finding clusters')
+        # codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+        # print('cluster centres:\n', codes)
+        #
+        # vecs, dist = scipy.cluster.vq.vq(ar, codes)  # assign codes
+        # counts, bins = scipy.histogram(vecs, len(codes))  # count occurrences
+        #
+        # index_max = scipy.argmax(counts)  # find most frequent
+        # peak = codes[index_max]
+        # colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
+        # print('most frequent is %s (#%s)' % (peak, colour))
 
 def clearUploads():
     folder = 'uploads'
